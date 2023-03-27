cb2b7626a2090d3765ca4f32fe9c14986b5a6ce5
PinguiSC <37206410+PinguiSC@users.noreply.github.com>
2023-03-27 16:12:05 +0200

    Add files via upload

---
+++ js/markers_num.js
@@ -0,0 +1,161 @@
+//BLIPS VERDES
+
+L.marker([-5005.000, 4007.500], { icon: cardIcon0 }).addTo(map)
+.bindPopup("<h3>A</h3><p><a href='https://i.imgur.com/zy9bkYx.jpg' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3535.000, 4896.500], { icon: cardIcon0 }).addTo(map)
+  .bindPopup("<h3>C</h3><p><a href='https://i.imgur.com/e86rQFY.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2256.500, 2770.000], { icon: cardIcon0 }).addTo(map)
+  .bindPopup("<h3>B</h3><p><a href='https://i.imgur.com/X1K9cwE.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+
+//BLIPS AZULES
+
+L.marker([-6926.250, 5253.875], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Palomino Highlands</h3><h6>Playa pintoresca</h6><p><a href='https://i.imgur.com/mnKulHT.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+L.marker([-6605.750, 5056.500], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Palomino Highlands</h3><h6>Cima de la cordillera Palomino Highlands</h6><p><a href='https://i.imgur.com/q2ctzd9.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+L.marker([-6470.50, 5485.00], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Palomino Highlands</h3><h6>Bahia de Palomino</h6><p><a href='https://i.imgur.com/xHQcNyh.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+L.marker([-6033.375, 5570.500], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cordillera Tataviam</h3><h6>Playa pintoresca</h6><p><a href='https://i.imgur.com/PC6smhf.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+L.marker([-5187.625, 5684.750], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cordillera Tataviam</h3><h6>Formación rocosa</h6><p><a href='https://i.imgur.com/bX9YMfX.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-5018.625, 4172.000], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Mirador de Marlowe Drive</h3><h6>Letreto de Vinewood</h6><p><a href='https://i.imgur.com/hOCrRqu.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4630.625, 4300.000], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Colina Vinewood</h3><h6>Vista del Condado de Blaine</h6><p><a href='https://i.imgur.com/jdO5dzl.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4768.625, 3528.50], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Observatorio Galileo</h3><h6>Vista de la Ciudad de Los Santos</h6><p><a href='https://i.imgur.com/8680wQV.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4485.5, 3468], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Arthur's Pass Trail</h3><h6>Ruta de montaña</h6><p><a href='https://i.imgur.com/aJ3fh9F.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-5036.5, 2850], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Marlowe Drive</h3><h6>Vista de la Ciudad de Los Santos</h6><p><a href='https://i.imgur.com/HOynz25.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4401.5, 2752], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Valle de Tongva</h3><h6>Afluente del río Tongva </h6><p><a href='https://i.imgur.com/6WFDyGS.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4848.5, 2125], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Colinas de Tongva</h3><h6>Valle Marlowe</h6><p><a href='https://i.imgur.com/Ja8GdUr.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4209.5, 2415], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Colinas de Tongva</h3><h6>Vista al viñedo Marlowe</h6><p><a href='https://i.imgur.com/cMmh2AP.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4151.5, 2683], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Valle de Tongva</h3><h6>Two Hoot Falls</h6><p><a href='https://i.imgur.com/AgRWeUx.jpg' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4257.5, 5076], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Granja eólica Ron Alternates</h3><h6>Vistas del Condado de Blaine</h6><p><a href='https://i.imgur.com/L87qvw9.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4107.5, 5791], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cordillera San Chianski</h3><h6>Cala de Chianski</h6><p><a href='https://i.imgur.com/Sry3ZCL.png ' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3371.5, 6171], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cordillera San Chianski</h3><h6>Laderas de San Chianski</h6><p><a href='https://i.imgur.com/ZAiih4M.png ' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4204.5, 2827], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Valle de Tongva </h3><h6>Cataratas del río Tongva</h6><p><a href='https://i.imgur.com/94WIq01.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3522.5, 3487], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Valle Zancudo </h3><h6>Rio Zancudo</h6><p><a href='https://i.imgur.com/Pb19NZF.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3260.5, 5017], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Desierto Grand Senora </h3><h6>Pedregal de Grand Senora</h6><p><a href='https://i.imgur.com/KJadDok.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3081.5, 5488], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Desierto Grand Senora</h3><h6>Mirador de Chilliad View</h6><p><a href='https://i.imgur.com/WmKeAgn.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2762.5, 3426], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Lago Alamo </h3><h6>Vistas panorámicas</h6><p><a href='https://i.imgur.com/g9YQvyi.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2678.5, 3259], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cañon Raton</h3><h6>Vistas panorámicas</h6><p><a href='https://i.imgur.com/Ena217N.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2456.5, 3016], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cañon Raton</h3><h6>Vistas panorámicas</h6><p><a href='https://i.imgur.com/S54r4tz.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2554.5, 2330], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Chumash Norte </h3><h6>Delta Chumash</h6><p><a href='https://i.imgur.com/NIGFR8L.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2043.5, 2509], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Chumash Norte </h3><h6>Archipiélago Chumash</h6><p><a href='https://i.imgur.com/zHNPDzM.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1531.5, 3170], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cabo Paleto </h3><h6>Vistas a Paleto Bay y al Monte Chilliad</h6><p><a href='https://i.imgur.com/E1SN3s8.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2000.5, 2850], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Chumash Norte</h3><h6>Cabo Chumash</h6><p><a href='https://i.imgur.com/4bRi8xQ.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-889.5, 3811], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Cabo Paleto </h3><h6>Vistas al Oceano Pacifico</h6><p><a href='https://i.imgur.com/x0mmtYp.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1204.5, 4742], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Monte Gordo </h3><h6>Playa Gordo</h6><p><a href='https://i.imgur.com/hbpp0a9.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1450.5, 5454], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Monte Gordo </h3><h6>Lagos naturales</h6><p><a href='https://i.imgur.com/Fi3caJB.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1589.5, 5608], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Monte Gordo </h3><h6>Vistas al Condado de Blaine</h6><p><a href='https://i.imgur.com/UmS8biK.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1590, 5795], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Monte Gordo </h3><h6>Lagos naturales</h6><p><a href='https://i.imgur.com/EgwF8MZ.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1860, 4081], { icon: cardIcon1 }).addTo(map)
+  .bindPopup("<h3>Monte Chilliad </h3><h6>Vistas al sur del Estado</h6><p><a href='https://i.imgur.com/Oh3dFNy.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+//BLIPS AMARILLOS
+
+L.marker([-5531.500, 4852.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Presa Tataviam Mountains</h3><h6>Vistas al lago y la ciudad</h6><p><a href='https://i.imgur.com/EGmjyVY.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4736.500, 4252.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Letrero de Vinewood</h3><h6>Mitico letrero de Vinewood</h6><p><a href='https://i.imgur.com/HfiDZPC.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+  L.marker([-4770, 3482.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Observatorio Galileo</h3><h6>Observatorio de San Andreas</h6><p><a href='https://i.imgur.com/0fw3Y7N.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4891, 1504.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Puerto de Chumash</h3><h6>Mitica pasarela del puerto de Chumash</h6><p><a href='https://i.imgur.com/6Qs136j.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4145, 2719.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Totem Two Hoots Falls</h3><h6>Vista cascadas</h6><p><a href='https://i.imgur.com/7MAkR99.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4143, 3356.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Great Chaparral</h3><h6>Mina Chaparral</h6><p><a href='https://i.imgur.com/eQWeK7e.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-4180, 5230.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Granja eólica Ron Alternates</h3><h6>Molinos</h6><p><a href='https://i.imgur.com/9r0qJwn.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3572, 3470.000], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Valle Zancudo</h3><h6>Puente Zancudo</h6><p><a href='https://i.imgur.com/0Hf958z.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3678, 3549.500 ], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Valle Zancudo</h3><h6>Iglesia Hill Valley</h6><p><a href='https://i.imgur.com/nA66V9a.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3611, 4441.500 ], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Desierto Grand Senora</h3><h6>Mina Grande</h6><p><a href='https://i.imgur.com/dgx076h.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-3043, 5396.500 ], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Desierto Grand Senora</h3><h6>Montaña de la salvación</h6><p><a href='https://i.imgur.com/dMPW4FT.png ' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2120, 6016.500 ], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Monte Gordo</h3><h6>Faro del este</h6><p><a href='https://i.imgur.com/Au3NyTP.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-2615, 3415.500 ], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Cañon Raton</h3><h6>Puente del ferrocarril</h6><p><a href='https://i.imgur.com/sdVRO36.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1571, 3461.500 ], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Paleto Bay</h3><h6>Estacion del BCSD</h6><p><a href='https://622386.smushcdn.com/1998349/wp-content/uploads/2022/01/default_image.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
+
+  L.marker([-1474, 3547.500 ], { icon: cardIcon2 }).addTo(map)
+  .bindPopup("<h3>Paleto Bay</h3><h6>Iglesia Bautista de Santa Brigida</h6><p><a href='https://622386.smushcdn.com/1998349/wp-content/uploads/2022/01/default_image.png' data-lightbox='image-1' data-title='Foto realizada por: Cheyenne Dawson'>Mostrar sitio</a></p></p>");
